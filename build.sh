2d6d436944f01953fb1aeb57e5f51feda5b60a21
sne4ker <101662671+sne4ker@users.noreply.github.com>
2022-10-12 19:53:53 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+# This script automatically builds for Windows, Darwin and Linux and outputs the executables into the release/ directory
+
 GOOS="windows" go build -ldflags "-s -w" -o release/fbWin.exe
 GOOS="darwin" go build -ldflags "-s -w" -o release/fbDarwin
 GOOS="linux" go build -ldflags "-s -w" -o release/fbLinux
